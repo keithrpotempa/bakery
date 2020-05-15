9056cb7bbb231875f24e673262121eeb3bad52cc
keithrpotempa <keithrpotempa@gmail.com>
2020-05-15 11:33:35 -0500

    COMPLETED REQUIREMENT 4

--- breadapp/views/details.py
+++ breadapp/views/details.py
@@ -29,6 +29,7 @@ def get_bread_ingredients(bread_id):
         
         db_cursor.execute("""
           SELECT
+            bi.id,
             i.name,
             bi.amount
           FROM breadapp_ingredient i
@@ -42,12 +43,14 @@ def create_bread_ingredient(cursor, row):
     _row = sqlite3.Row(cursor, row)
 
     ingredient = Ingredient()
+    ingredient.bread_ingredient_id = _row["id"]
     ingredient.name = _row["name"]
     ingredient.amount = _row["amount"]
 
     return ingredient
 
 def bread_details(request, bread_id):
+    
     if request.method == 'GET':
         bread = get_bread(bread_id)
         ingredients = get_bread_ingredients(bread_id)
@@ -59,3 +62,25 @@ def bread_details(request, bread_id):
         } 
         
         return render(request, template, context)
+      
+    # Check if this POST is for deleting a bread_ingredient
+    elif request.method == "POST":
+        form_data = request.POST
+        
+        if (
+            "actual_method" in form_data
+            # The DELETE value also has the ingredient_bread_id
+            # so we need to split it and get the first part
+            and form_data["actual_method"].split("--")[0] == "DELETE"
+        ):
+            bread_ingredient_id = form_data["actual_method"].split("--")[1]
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE FROM breadapp_breadingredient
+                WHERE id = ?
+                """, (bread_ingredient_id,))
+            # This args is the bread id to pass into the path
+            # https://docs.djangoproject.com/en/3.0/ref/urlresolvers/#django.urls.reverse
+            return redirect(reverse('breadapp:bread', args=[bread_id]))
